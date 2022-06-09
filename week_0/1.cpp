85f83497406383fe24f7490a71a8bfef677e4ca1
Sangsthita Panda <88998179+sangsthita@users.noreply.github.com>
2022-06-09 21:55:02 +0530

    Create 1.cpp

---
+++ week_0/1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int count;
+void linear_search(vector<int>,int,int);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> arr;
+        for(int i=0;i<n;i++)
+        {
+           int a;
+           cin>>a;
+           arr.push_back(a);
+        }
+        int key;
+
+        cin>>key;
+        linear_search(arr,key,n);
+        ::count=0;
+    }
+
+    return 0;
+}
+
+void linear_search(vector<int>arr,int key,int n)
+{
+    for(int i=0;i<n;i++)
+        { ::count++;
+            if(key==arr[i])
+            {   
+
+                cout<<"Present "<<::count<<endl;
+                return;
+            }
+        }
+
+            cout<<"Not Present "<<::count<<endl;
+
+}
